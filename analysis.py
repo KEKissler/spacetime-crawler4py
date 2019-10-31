e5dd0ab35cd8e03c29dbd0593f6a623346fcef7f
CraftService <jealmend@uci.edu>
2019-10-30 17:24:25 -0700

    add analysis.py

---
+++ analysis.py
@@ -0,0 +1,36 @@
+
+#return the number of unique pages
+def uniquePages():
+
+    numPages = 0
+
+    #read from file, frontier.shelve.urls.txt
+    #? put into a set()
+    #return size of set()
+
+    return numPages
+
+
+#return the url of the longest page
+def maxWordCount():
+
+    wordCount = 0
+    #keep a global maxWordCount variable that gets updated by tokenize() import PartA.py
+    return wordCount
+
+
+#return a list of the most common words
+def mostCommonWords():
+
+    list words []
+    #insert into dictionary
+    #sort by key-value
+    #copy/print first 50 valuesOB
+    return words
+
+#return list of subdomains, ordered alphabetically with 
+#number of unique pages detected in each subdomain
+def listSubdomains():
+
+    
+    return None
