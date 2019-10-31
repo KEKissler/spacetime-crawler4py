e32b6882fd68cd470b6d55a87a5abc4ffb984239
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-10-30 17:46:17 -0700

    Add soup

--- scraper.py
+++ scraper.py
@@ -22,32 +22,29 @@ def scraper(url, resp):
         return list()
     if resp.status >= 600 and resp.status <= 608:
         return list()
-
-    links = extract_next_links(url, resp)
+    if resp.status == 200 and resp.raw_response is None:
+        return list()
     
-    #return [link for link in links if is_valid(link)]
-    
+    soup = BeautifulSoup(resp.raw_response.text.encode("utf-8"), features="lxml")
+    links = extract_next_links(url, soup)
+    contentText = ""
+    for contentGroup in soup.find_all(['p', 'title', re.compile(r"^h[0-9]+$")]):
+        for string in contentGroup.stripped_strings:
+            contentText += string + ' '
     urlsFile = open("urls.txt","a+")
 
+    result = []
     for link in links:
         if is_valid(link):
-            #openfile and print url, tokenCount = ?
-            urlsFile.write(link + " " + tokenize(?))
-            #call tokenize
-            
+            urlsFile.write(link + " " + tokenize(contentText))
+            result.append(link)
         else:
-            #openfile and print url, tokenCount = -1
             urlsFile.write(link + " " + "-1")
 
-    return token;
+    return result;
 
-def extract_next_links(url, resp):
-    a = list()
-    #soup = BeautifulSoup(resp.raw_response.content, features="lxml")
-    soup = BeautifulSoup(resp.raw_response.text.encode("utf-8"), features="lxml")
-    for link in soup.find_all('a'):
-        a.append(normalize_link(url, link.get('href')))
-    return a
+def extract_next_links(url, soup):
+    return [normalize_link(url, link.get('href')) for link in soup.find_all('a')]
 
 def normalize_link(current_link, new_link):
     parsed = urlparse(new_link)
@@ -85,7 +82,8 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
-	        + r"|pdf|pdfs|css|js|ppts|exe|o"
+	    + r"|pdf|pdfs|css|js|ppts|exe|o"
+            + r"tvs|bgz|tbi"
             + r"|war|apk|sql|ppsx|pptx)$", parsed.path.lower())
 
     except TypeError:
